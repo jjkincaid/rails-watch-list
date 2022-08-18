2a4d73a4fb09515bea27f7c33f202f5fd8cf6d33
Jesica Kincaid <jesica.j.kincaid@gmail.com>
2022-08-18 13:07:45 +0200

    watchlist steps 1-3

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,23 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+
+require 'faker'
+
+puts 'Creating movies'
+Movie.destroy_all
+
+5.times do
+  title = Faker::Movie.title
+  overview = Faker::Lorem.paragraph
+  poster_url = Faker::Internet.url
+  rating = Faker::Number.between(from: 0, to: 10)
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
+
+puts 'Finished!'
+
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
